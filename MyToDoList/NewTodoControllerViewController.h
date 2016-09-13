abd9f607b4cc2337c89a4e17b6cd74ce16a666f5
Akila <akila@Mob3.local>
2016-09-13 18:17:30 +0530

    First Commit

---
+++ MyToDoList/NewTodoControllerViewController.h
@@ -0,0 +1,13 @@
+//
+//  NewTodoControllerViewController.h
+//  MyToDoList
+//
+//  Created by Akila on 9/7/16.
+//  Copyright © 2016 Akila. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewTodoControllerViewController : UIViewController
+
+@end
---
+++ MyToDoList/NewTodoControllerViewController.m
@@ -0,0 +1,37 @@
+//
+//  NewTodoControllerViewController.m
+//  MyToDoList
+//
+//  Created by Akila on 9/7/16.
+//  Copyright © 2016 Akila. All rights reserved.
+//
+
+#import "NewTodoControllerViewController.h"
+
+@interface NewTodoControllerViewController ()
+
+@end
+
+@implementation NewTodoControllerViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
